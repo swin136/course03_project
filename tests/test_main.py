fd191d4ccf7a5dffc40bcd47facd03fc51aaeb35
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-05 19:08:50 +0300

    modify tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,19 @@
 from src.main import load_operations
+from src.main import filter_operations
 
 
-def test_load():
+def test_load_no_file():
     assert load_operations('no_file') == None
+
+def test_load_error_json():
+    assert load_operations('tests/operations_error.json') == None
+
+def test_load_normal_json():
+    test_list = load_operations('tests/operations_main.json')
+    assert len(test_list) == 100
+    assert filter_operations(test_list, 5) == 5
+    # assert len(load_operations('tests/operations_err_record.json')) == 99
+
+def test_load_empty():
+    assert load_operations('tests/empty.json') == []
+

85ae9a818244619c5ba2e982f69998e5582fc64e
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-05 21:15:18 +0300

    modify testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,30 @@
+import pytest
+
 from src.main import load_operations
 from src.main import filter_operations
+
+@pytest.fixture
+def get_root_dir():
+
+    """
+    При запуске тестов из PyCharm возврат функции должен быть: return ""
+    При запуске тестов из терминала в корне проекта возврат функции должен быть: return "tests\\" или return "tests\\"
+    """
+    return "tests\\"
 
 
 def test_load_no_file():
     assert load_operations('no_file') == None
 
-def test_load_error_json():
-    assert load_operations('tests/operations_error.json') == None
+def test_load_error_json(get_root_dir):
+    assert load_operations(get_root_dir + 'operations_error.json') == None
 
-def test_load_normal_json():
-    test_list = load_operations('tests/operations_main.json')
+def test_load_normal_json(get_root_dir):
+    test_list = load_operations(get_root_dir + 'operations_main.json')
     assert len(test_list) == 100
     assert filter_operations(test_list, 5) == 5
-    # assert len(load_operations('tests/operations_err_record.json')) == 99
+    assert len(load_operations(get_root_dir + 'operations_errors_2.json')) == 98
 
-def test_load_empty():
-    assert load_operations('tests/empty.json') == []
+def test_load_empty(get_root_dir):
+    assert load_operations(get_root_dir + 'empty.json') == []
 

666eaec1d37092af1d321957b1064784580f6a16
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-06 09:50:58 +0300

    reformat text PyCharm

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import pytest
-
 from src.main import load_operations
 from src.main import filter_operations
+
 
 @pytest.fixture
 def get_root_dir():
@@ -14,10 +14,12 @@ def get_root_dir():
 
 
 def test_load_no_file():
-    assert load_operations('no_file') == None
+    assert load_operations('no_file') is None
+
 
 def test_load_error_json(get_root_dir):
-    assert load_operations(get_root_dir + 'operations_error.json') == None
+    assert load_operations(get_root_dir + 'operations_error.json') is None
+
 
 def test_load_normal_json(get_root_dir):
     test_list = load_operations(get_root_dir + 'operations_main.json')
@@ -25,6 +27,6 @@ def test_load_normal_json(get_root_dir):
     assert filter_operations(test_list, 5) == 5
     assert len(load_operations(get_root_dir + 'operations_errors_2.json')) == 98
 
+
 def test_load_empty(get_root_dir):
     assert load_operations(get_root_dir + 'empty.json') == []
-
